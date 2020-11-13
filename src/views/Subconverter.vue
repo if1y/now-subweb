ba771982e565dd5d03edfac4886a9cee0a44e443
yesmiley <32222791+yesmiley@users.noreply.github.com>
2020-11-14 01:02:44 +0800

    Back to NoAdblock

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -587,7 +587,7 @@ export default {
   mounted() {
     this.form.clientType = "clash&new_name=true";
     this.form.customBackend = "https://subyes.vercel.app/sub?";
-    this.form.remoteConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_AdblockPlus.ini";
+    this.form.remoteConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online.ini";
     this.getBackendVersion();
   },
   methods: {
